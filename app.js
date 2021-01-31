5e5a471743a5fb0269f36368747fe8e19b255e3a
Barzi Jaf <barzi4greatness@gmail.com>
2021-01-31 22:20:55 +0100

    Not working solution :(

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 const cells = document.querySelectorAll('.cell');
-let isX = true;
+let playerTurn = "x";
+
+let winPositions = [[1,5,9],[1,2,3],[1,4,7],[7,8,9],[3,6,9],[2,5,8],[4,5,6]];
+
+let xPositions = [];
+let oPositions = [];
+
+let x = 0;
+
 
 cells.forEach(cell => {
   cell.addEventListener('click', ()=> {
+    const cellNumber = parseInt(cell.dataset.indexNumber);
+    
     if(cell.classList.contains('x')) {
-      isX = true;
+      return;    
     } else if(cell.classList.contains('o')) {
-      isX = false;
+      return;
     }
-    if(isX == true) {
-      cell.classList.add('x');
-      isX = false;
-    } else if(isX == false){
-      cell.classList.add('o');
-      isX = true;
-    }
+
+    if(playerTurn == "x") {
+      cell.classList.add(playerTurn);
+      playerTurn = "o";
+      xPositions.push(cellNumber);
+
+      let set = 0;
+      while (x < winPositions.length) {
+        if(winPositions[set].includes(xPositions[x])) {
+          console.log("included, x is", x, "|set is", set, "|xPos is", xPositions[x]);
+          x++;
+          return;
+        }
+        else {
+          set++;
+          console.log("not included, x is", x);
+        }
+        console.log("x is", x);
+        
+        if(set == 7) {
+          console.log("set is",set);
+          set = 0;
+          return;
+        }
+      }
+    } else if(playerTurn == "o"){
+      cell.classList.add(playerTurn);
+      playerTurn = "x";
+      oPositions.push(cellNumber);
+    } 
   })
 })
